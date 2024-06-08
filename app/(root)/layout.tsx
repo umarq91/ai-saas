da8a292e198ce10b5b9c4efa6e7ba13852637512
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2024-06-08 09:28:49 +0500

    HomePage Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
 import LeftSideBar from "@/components/LeftSideBar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -7,11 +10,27 @@ export default function RootLayout({
 }>) {
   return (
   <div>
-    <main className="">
+    <main className="relative flex bg-black-3">
     <LeftSideBar/>
-      {children}
-      <p className="text-white-1">Right</p>
+    <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
+              <MobileNav />
+            </div>
+            <div className="flex flex-col md:pb-14">
+              {/* <Toaster /> */}
 
+              {children}
+            </div>
+          </div>
+        </section>
+<RightSidebar/>
     </main>
   </div>     
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,26 @@
+import PodcastCard from '@/components/PodcastCard'
 import React from 'react'
+import { podcastData } from "@/constants"
 
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
-    <section className='flex flex-col '>
-      <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-    </section>
-  </div>
+      <section className='flex flex-col gap-5'>
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+
+        <div className="podcast_grid">
+          {podcastData?.map(({ id,title,description,imgURL }) => (
+            <PodcastCard
+              key={id}
+              imgUrl={imgURL as string}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
 
